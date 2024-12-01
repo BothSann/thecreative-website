2dfa95c7de8e758850b2b4e09e02d86edd03ce6a
Both Sann <thannsopheakboth@gmail.com>
2024-12-01 14:53:34 +0700

    first commit

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 "use strict";
+const storyBtn = document.querySelectorAll(".story");
+console.log(storyBtn);
+
+storyBtn.forEach(function (btn) {
+  btn.addEventListener("click", function () {
+    btn.classList.toggle("show");
+    btn.nextElementSibling.classList.toggle("show");
+  });
+});
